0cd75e724e52cc070104d3fe8af2312fd4262796
valentin.i.petraru <valentin.i.petraru@accenture.com>
2023-05-22 14:31:14 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BtnBlueComponent } from './buttons/btn-blue/btn-blue.component';
 import { BtnBasicComponent } from './buttons/btn-basic/btn-basic.component';
+import { PayCardComponent } from './cards/pay-card/pay-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     BtnBlueComponent,
-    BtnBasicComponent
+    BtnBasicComponent,
+    PayCardComponent
   ],
   imports: [
     BrowserModule,
